150124f13389f4f1d598b9609f1478d43aab02f8
Nat-Rubin <nathanielrubinm@gmail.com>
2023-11-22 15:30:49 -0500

    It works now on Arch Linux (konsole)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{fs, io, thread, time};
-use std::path::PathBuf;
 use std::process::{Child, Command};
 use libc::pid_t;
 
@@ -11,13 +10,7 @@ fn continue_process(id: u32) {
     unsafe { libc::kill(id as pid_t, libc::SIGCONT); }
 }
 fn main() {
-    let time = time::Duration::from_millis(5);
-    let interrupt = time::Duration::from_millis(20);
-
-    // let job0: [i32; 1000] = [0; 1000];
-    // let job1: [i32; 1000] = [1; 1000];
-    // let job2: [i32; 1000] = [2; 1000];
-    // let jobs: Vec<[i32; 1000]> = vec![job0, job1, job2];
+    let interrupt = time::Duration::from_millis(1);
 
     let jobs = fs::read_dir("jobs").unwrap()
         .map(|res| res.map(|e| e.path()))
@@ -29,16 +22,19 @@ fn main() {
     for job in &jobs {
         // creates a.txt new child process in a.txt paused state and adds it to the vec of processes
         // TODO: open in new terminal
-        child = Command::new(job).spawn().expect("Failed");
+        let job_command = format!("./{}", job.to_string_lossy().to_string());
+        println!("{}", job_command);
+        child = Command::new("konsole").arg("--separate").arg("--hold").arg("-e").arg(job_command).spawn().expect("Failed");
         pause_process(child.id());
         processes.push(child);
     };
     loop {
         for child in &processes {
             continue_process(child.id());
+            println!("Started process {}", child.id());
             thread::sleep(interrupt);
             pause_process(child.id());
-            thread::sleep(interrupt);
+            println!("Stopped process {}", child.id());
         }
     }
 }
